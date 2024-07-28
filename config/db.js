bf617aad789d11f027472b3c7f171b7750c8e447
rhitambanerjee <rb2403@it.jgec.ac.in>
2024-07-29 01:05:22 +0530

    added signup and signin

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const conn = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGOURI);
+        console.log('Connection established successfully');
+    }
+    catch(e){
+        console.log(e.message);
+    }
+}
+
+module.exports = conn;
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const conn = require("./config/db");
+const userRouter = require('./routes/userRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+conn();
+
+app.use('/user',userRouter);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
